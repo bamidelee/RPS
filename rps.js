c804425684e8ec7dcfa2962d0093ffbeda95917d
bamidele <ibrahimolujide@gmail.com>
2022-06-17 18:33:02 +0100

    added a function to compare the options. added the rounds of play

--- rps.js
+++ rps.js
@@ -1,9 +1,30 @@
 // creating the three possible outputs for the game 
 const options = ['ROCK', 'PAPER', 'SCISSORS'];
-// randomly returning one of the possible options 
+
+// randomly returning one of the possible options to be the computer option
 let gamePlay =  opt => 
 {
-    return options[Math.floor(Math.random() * 3)];
-  
+    return options[Math.floor(Math.random() * 3)]; 
 }
-gamePlay(options);
+const computerOPtion = gamePlay(options);
+// comparing players option to computers option to declare the winner
+let battle = (playerOption, compOption) => 
+{
+    if (playerWin())
+    {
+        return `You win! ${playerOption} beats ${compOption}`;
+    }
+    else
+    {
+        return `you lose! ${compOption} beats ${playerOption}`;
+    }
+}
+
+// creating five rounds if play
+let playRound = () => 
+{
+    for (i = 0; i < 5; i++)
+    {
+        battle(playerOPtion, computerOPtion);
+    }
+}
